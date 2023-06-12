4ac18b5e1db7fa247a7a0b9de9cbcd2d62f40f58
Chance Jolley <chancejolley@msn.com>
2023-06-11 18:52:05 -0700

    adjusted a few things on the fishing game

--- src/main/java/com/fishekai/view/FishingFrame.java
+++ src/main/java/com/fishekai/view/FishingFrame.java
@@ -25,9 +25,6 @@ public class FishingFrame extends JFrame {
         this.gp = gp;
         this.statusPanel = gp.getFishekai().window.getStatusPanel();
 
-       
-
-
         setTitle("Fishing Game");
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
@@ -36,15 +33,38 @@ public class FishingFrame extends JFrame {
         contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
 
         // Create components
-        fishUpdateLabel = new JLabel();
-        lineTightLabel = new JLabel();
-        fishDistanceLabel = new JLabel();
+        fishUpdateLabel = new JLabel("", SwingConstants.CENTER);
+        lineTightLabel = new JLabel("", SwingConstants.CENTER);
+        fishDistanceLabel = new JLabel("", SwingConstants.CENTER);
         pullButton = new JButton("Pull");
         releaseButton = new JButton("Release");
 
+        // Set the font for the labels and buttons
+        Font labelFont = new Font("Arial", Font.PLAIN, 20);
+        fishUpdateLabel.setFont(labelFont);
+        lineTightLabel.setFont(labelFont);
+        fishDistanceLabel.setFont(labelFont);
+        pullButton.setFont(labelFont);
+        releaseButton.setFont(labelFont);
+
+        // Set the color for the labels and buttons
+        Color bgColor = Color.decode("#00827f");
+        Color fgColor = Color.WHITE;
+        fishUpdateLabel.setForeground(fgColor);
+        lineTightLabel.setForeground(fgColor);
+        fishDistanceLabel.setForeground(fgColor);
+        pullButton.setForeground(fgColor);
+        releaseButton.setForeground(fgColor);
+        contentPanel.setBackground(bgColor);
+        pullButton.setBackground(bgColor);
+        releaseButton.setBackground(bgColor);
+
         // Add components to the content panel
         contentPanel.add(fishUpdateLabel);
+        contentPanel.add(new JLabel(" "));
         contentPanel.add(lineTightLabel);
+        contentPanel.add(new JLabel(" "));
+        contentPanel.add(new JLabel(" "));
         contentPanel.add(fishDistanceLabel);
 
         // Create panel for buttons
@@ -58,9 +78,9 @@ public class FishingFrame extends JFrame {
 
         // Set initial text for the labels
         fishUpdateLabel.setText("You cast your line into the water and wait patiently...");
-        lineTightLabel.setText("Is the line tight? " + fishingMechanic.isLineTight());
+        String isLineTight = fishingMechanic.isLineTight() ? "Yes" : "No";
+        lineTightLabel.setText("Is the line tight? " + isLineTight);
         fishDistanceLabel.setText("Fish distance: " + fishingMechanic.updateFishDistance());
-
 
         // Attach action listeners to the buttons
         pullButton.addActionListener(new ActionListener() {
@@ -68,7 +88,8 @@ public class FishingFrame extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 String message = fishingMechanic.pullLine();
                 fishUpdateLabel.setText(message);
-                lineTightLabel.setText("Is the line tight? " + fishingMechanic.isLineTight());
+                String isLineTight = fishingMechanic.isLineTight() ? "Yes" : "No";
+                lineTightLabel.setText("Is the line tight? " + isLineTight);
                 fishDistanceLabel.setText("Fish distance: " + fishingMechanic.updateFishDistance());
 
                 // Check if fish is caught
@@ -85,18 +106,19 @@ public class FishingFrame extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 String message = fishingMechanic.releaseLine();
                 fishUpdateLabel.setText(message);
-                lineTightLabel.setText("Is the line tight? " + fishingMechanic.isLineTight());
+                String isLineTight = fishingMechanic.isLineTight() ? "Yes" : "No";
+                lineTightLabel.setText("Is the line tight? " + isLineTight);
                 fishDistanceLabel.setText("Fish distance: " + fishingMechanic.updateFishDistance());
             }
         });
 
         // Set the size and make the frame visible
-        setSize(500, 200);
+        setSize(600, 300);
         setLocationRelativeTo(null);
         setVisible(true);
     }
+
     private void handleFishCaught() {
-        // TODO: Implement the logic for when the player successfully catches the fish JOSHO
         String fishType = fishingMechanic.getCaughtFish().getName();
         OBJ_Caught_Fish fish = new OBJ_Caught_Fish(fishType);
         gp.getPlayer().getInventory().add(fish);
@@ -112,7 +134,6 @@ public class FishingFrame extends JFrame {
     }
 
     private void handleFishEscaped() {
-
         JOptionPane.showMessageDialog(this, "The fish escaped. Better luck next time!", "Fish Escaped", JOptionPane.INFORMATION_MESSAGE);
         gp.setPaused(false);
         dispose();
@@ -121,9 +142,7 @@ public class FishingFrame extends JFrame {
     protected void processWindowEvent(WindowEvent e) {
         super.processWindowEvent(e);
         if(e.getID() == WindowEvent.WINDOW_CLOSING) {
-            // Move the character off the dock:
             gp.setPaused(false);
         }
     }
 }
-
